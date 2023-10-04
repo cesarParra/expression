24f8e90ff2470235cc4eb50582eec7f2aae4f564
Cesar Parra <cesar.parra@gmail.com>
2023-10-04 16:51:44 -0400

    Implementing Expression evaluator so that the Nav Bar takes its information out of the expression result

--- expression-components/main/ui/lwc/hero/hero.js
+++ expression-components/main/ui/lwc/hero/hero.js
@@ -1,6 +1,51 @@
 import TwElement from "c/twElement";
-import {api} from "lwc";
+import evaluate from '@salesforce/apex/FormulaEvaluatorUiController.evaluate';
+import { api, wire } from "lwc";
 
 export default class Hero extends TwElement {
   @api expr;
+
+  computed;
+  error;
+
+  @wire(evaluate, {recordId: '', formula: '$expr'})
+  evaluate({error, data}) {
+    if (error) {
+      console.error(error);
+      this.error = error.body.message;
+    } else {
+      this.computed = data;
+      this._validate();
+    }
+  }
+
+  // callToAction() {
+  //   window.open(this.computed.callToAction.url, '_self');
+  // }
+  //
+  // secondaryAction() {
+  //   window.open(this.computed.secondaryAction.url, '_self');
+  // }
+
+  _validate() {
+    if (!this.computed) {
+      return;
+    }
+    // Computed should contain a "title" property.
+    if (!("title" in this.computed)) {
+      this.error = 'Hero component requires a title.';
+    }
+  }
+
+  get loading() {
+    return !this.computed && !this.error;
+  }
+
+  get hasError() {
+    return this.error;
+  }
+
+  get ready() {
+    return !this.loading && !this.hasError;
+  }
 }
