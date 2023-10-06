dfe0b5a44a2802727b29a3fb9266e5942ad156d7
Cesar Parra <cesar.parra@gmail.com>
2023-10-06 14:59:37 -0400

    Logos - initial commit.

---
+++ expression-components/main/ui/lwc/logos/logos.js
@@ -0,0 +1,48 @@
+import TwElement from "c/twElement";
+import evaluate from '@salesforce/apex/FormulaEvaluatorUiController.evaluate';
+import { api, wire } from "lwc";
+
+export default class Logos extends TwElement {
+  @api expr;
+
+  computed;
+  error;
+
+  @wire(evaluate, {recordId: '', formula: '$expr'})
+  evaluate({error, data}) {
+    if (error) {
+      console.error(error);
+      this.error = error.body.message;
+    } else {
+      this.computed = data;
+      this._validate();
+    }
+  }
+
+  get loading() {
+    return !this.computed && !this.error;
+  }
+
+  get hasError() {
+    return this.error;
+  }
+
+  get ready() {
+    return !this.loading && !this.hasError;
+  }
+
+  _validate() {
+    if (!this.computed) {
+      return;
+    }
+    // Computed should contain a "title" property.
+    if (!("title" in this.computed)) {
+      this.error = 'Hero component requires a title.';
+    }
+
+    // Computed should contain a "logos" property.
+    if (!("logos" in this.computed)) {
+      this.error = 'Hero component requires a "logos" key.';
+    }
+  }
+}
