996c06df2a13a6ebc3fdf3603260cb8eff63bebe
Cesar Parra <cesar.parra@gmail.com>
2023-10-26 06:24:19 -0400

    Initial commit

--- expression-components/main/ui/components/exposed/lwc/features/features.js
+++ expression-components/main/ui/components/exposed/lwc/features/features.js
@@ -1,15 +1,29 @@
 import TwElement from "c/twElement";
 import evaluate from '@salesforce/apex/FormulaEvaluatorUiController.evaluate';
+import { CurrentPageReference } from "lightning/navigation";
 import { api, wire } from "lwc";
 
+// TODO: There's going to be way too much duplicate code now, we need to find a way to share whatever possible
 export default class Features extends TwElement {
+  @api contextUrlParam;
+  @api previewContextId;
   @api expr;
   @api respectSharing;
 
+  currentPageReference;
   computed;
   error;
+  contextId = null
 
-  @wire(evaluate, {recordId: '', formula: '$expr', respectSharing: '$respectSharing'})
+  @wire(CurrentPageReference)
+  setCurrentPageReference(currentPageReference) {
+    this.currentPageReference = currentPageReference;
+    if (this.contextUrlParam) {
+      this.contextId = this.isInBuilder ? this.previewContextId : this.currentPageReference.state[this.contextUrlParam];
+    }
+  }
+
+  @wire(evaluate, {recordId: '$contextId', formula: '$expr', respectSharing: '$respectSharing'})
   evaluate({error, data}) {
     if (error) {
       console.error(error);
@@ -29,11 +43,33 @@ export default class Features extends TwElement {
   }
 
   get ready() {
-    return !this.loading && !this.hasError;
+    return !this.loading && !this.hasError && !this.cannotShowPreview;
   }
 
   get hasText() {
     return this.computed?.title || this.computed?.description;
+  }
+
+  get toDelete() {
+    return JSON.stringify(this.currentPageReference, null, 2);
+  }
+
+  get isInBuilder() {
+    return this.currentPageReference?.state?.app === 'commeditor';
+  }
+
+  get cannotShowPreview() {
+    // If in the builder, and the contextUrlParam is set but the previewContextId is not,
+    // then we cannot show the preview.
+    if (!this.contextUrlParam) {
+      return false;
+    }
+
+    if (this.isInBuilder) {
+      return !this.previewContextId;
+    }
+
+    return false;
   }
 
   _validate() {
