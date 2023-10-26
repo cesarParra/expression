20acbaf23252a41811feca443e647f019b5c8b30
Cesar Parra <cesar.parra@gmail.com>
2023-10-26 16:02:24 -0400

    Adding context URL para support to the NavBar component

--- expression-components/main/ui/components/exposed/lwc/navBar/navBar.js
+++ expression-components/main/ui/components/exposed/lwc/navBar/navBar.js
@@ -1,42 +1,15 @@
-import TwElement from "c/twElement";
-import evaluate from '@salesforce/apex/FormulaEvaluatorUiController.evaluate';
-import {api, wire} from "lwc";
+import ExpressionSiteElement from "c/expressionSiteElement";
+import { api } from "lwc";
 
-export default class NavBar extends TwElement {
+export default class NavBar extends ExpressionSiteElement {
+  @api contextUrlParam;
+  @api previewContextId;
   @api expr;
   @api respectSharing;
 
   isOpen = false;
-  computed;
-  error;
-
-  @wire(evaluate, {recordId: '', formula: '$expr', respectSharing: '$respectSharing'})
-  evaluate({error, data}) {
-    if (error) {
-      console.error(error);
-      this.error = error.body.message;
-    } else {
-      this.computed = data;
-    }
-  }
 
   toggle() {
     this.isOpen = !this.isOpen;
-  }
-
-  callToAction() {
-    window.open(this.computed.callToAction.url, '_self');
-  }
-
-  get loading() {
-    return !this.computed && !this.error;
-  }
-
-  get hasError() {
-    return this.error;
-  }
-
-  get ready() {
-    return !this.loading && !this.hasError;
   }
 }
