f238f30faf75aa452bd8a7f49675ebf720ff6421
Cesar Parra <cesar.parra@gmail.com>
2023-10-26 18:00:15 -0400

    Adding context URL para support to the Stats component

--- expression-components/main/ui/components/exposed/lwc/stats/stats.js
+++ expression-components/main/ui/components/exposed/lwc/stats/stats.js
@@ -1,43 +1,17 @@
-import TwElement from "c/twElement";
-import evaluate from '@salesforce/apex/FormulaEvaluatorUiController.evaluate';
-import { api, wire } from "lwc";
-import { classNames } from 'c/utils';
+import ExpressionSiteElement from "c/expressionSiteElement";
+import { api } from "lwc";
 
-export default class Status extends TwElement {
+export default class Status extends ExpressionSiteElement {
+  @api contextUrlParam;
+  @api previewContextId;
   @api expr;
   @api respectSharing;
-
-  computed;
-  error;
-
-  @wire(evaluate, {recordId: '', formula: '$expr', respectSharing: '$respectSharing'})
-  evaluate({error, data}) {
-    if (error) {
-      console.error(error);
-      this.error = error.body.message;
-    } else {
-      this.computed = data;
-      this._validate();
-    }
-  }
 
   get hasText() {
     return this.computed?.title || this.computed?.description;
   }
 
-  get loading() {
-    return !this.computed && !this.error;
-  }
-
-  get hasError() {
-    return this.error;
-  }
-
-  get ready() {
-    return !this.loading && !this.hasError;
-  }
-
-  _validate() {
+  validate() {
     if (!this.computed) {
       return;
     }
