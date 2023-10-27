1b78979bbd2d27ff80ea243e13775f3cc3f470d2
Cesar Parra <cesar.parra@gmail.com>
2023-10-27 11:31:20 -0400

    Displaying stencils while the components load (#76)
    * Displaying stencils while the components load
    
    * Package version release.

--- expression-components/main/ui/components/base/lwc/expressionElementContainer/expressionElementContainer.js
+++ expression-components/main/ui/components/base/lwc/expressionElementContainer/expressionElementContainer.js
@@ -3,4 +3,5 @@ import { LightningElement, api } from 'lwc';
 export default class ExpressionElementContainer extends LightningElement {
   @api cannotShowPreview = false;
   @api error = null;
+  @api loading = false;
 }
