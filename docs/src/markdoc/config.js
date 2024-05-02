abf742806ca82edc0d0ee620004acdb29a7f6a0f
cesarParra <cesar.parra@gmail.com>
2024-05-01 21:07:37 -0400

    Releasing package version

--- docs/src/markdoc/config.js
+++ docs/src/markdoc/config.js
@@ -4,13 +4,13 @@ import nodes from "./nodes";
 const IS_PROD = process.env.NODE_ENV === 'production'
 
 const variables = {
-    tags,
-    nodes,
-    variables: {
-        packageId: "04tRb000000uSuTIAU",
-        componentPackageId: "04tRb000000sGz3IAE",
-        urlPrefix: IS_PROD ? "/expression" : "",
-    }
+  tags,
+  nodes,
+  variables: {
+    packageId: "04tRb000000yUJNIA2",
+    componentPackageId: "04tRb000000sGz3IAE",
+    urlPrefix: IS_PROD ? "/expression" : "",
+  }
 }
 
 export default variables;
