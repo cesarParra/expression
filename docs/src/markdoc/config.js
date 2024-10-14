32e53fce1c049a728cbe5ee602c1134320384c0c
Cesar Parra <cesar.parra@gmail.com>
2024-10-14 07:52:32 -0400

    Text format fix (#164)
    * The TEXT function no longer formats string values
    
    * Releasing a new package version

--- docs/src/markdoc/config.js
+++ docs/src/markdoc/config.js
@@ -7,7 +7,7 @@ const variables = {
   tags,
   nodes,
   variables: {
-    packageId: "04tRb000001iDH7IAM",
+    packageId: "04tRb000001sOuPIAU",
     componentPackageId: "04tRb0000012Mv8IAE",
     urlPrefix: IS_PROD ? "/expression" : "",
   }
