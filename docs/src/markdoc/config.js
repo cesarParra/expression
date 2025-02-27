2bc6349d33ac5dfb1e82e787d8eed76b48025f10
cesarParra <cesar.parra@gmail.com>
2025-02-27 09:47:19 -0400

    Releasing version 1.16 of the core language

--- docs/src/markdoc/config.js
+++ docs/src/markdoc/config.js
@@ -7,7 +7,7 @@ const variables = {
   tags,
   nodes,
   variables: {
-    packageId: "04tRb000002br5pIAA",
+    packageId: "04tRb000002dQ1VIAU",
     componentPackageId: "04tRb0000012Mv8IAE",
     urlPrefix: IS_PROD ? "/expression" : "",
   }
