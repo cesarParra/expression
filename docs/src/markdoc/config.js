5e83898144bb83fe5d14a8c55032cf7a946b01fc
cesarParra <cesar.parra@gmail.com>
2025-02-28 08:18:10 -0400

    Releasing new core package version

--- docs/src/markdoc/config.js
+++ docs/src/markdoc/config.js
@@ -7,7 +7,7 @@ const variables = {
   tags,
   nodes,
   variables: {
-    packageId: "04tRb000002dQ1VIAU",
+    packageId: "04tRb000002drePIAQ",
     componentPackageId: "04tRb0000012Mv8IAE",
     urlPrefix: IS_PROD ? "/expression" : "",
   }
