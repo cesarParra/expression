c57c66a649976afe93d786a41dcd83f659a1e8be
cesarParra <cesar.parra@gmail.com>
2025-04-08 06:12:50 -0400

    Making the Packages Id be a public JSON file

--- docs/src/markdoc/config.js
+++ docs/src/markdoc/config.js
@@ -1,5 +1,6 @@
 import tags from './tags'
 import nodes from './nodes'
+import packages from '../../public/packages.json'
 
 const IS_PROD = process.env.NODE_ENV === 'production'
 
@@ -7,8 +8,7 @@ const variables = {
   tags,
   nodes,
   variables: {
-    packageId: '04tRb000002mNZ3IAM',
-    componentPackageId: '04tRb0000012Mv8IAE',
+    ...packages,
     urlPrefix: IS_PROD ? '/expression' : '',
   },
 }
