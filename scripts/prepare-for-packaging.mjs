d4f5de5a33ee21b26fabf82edda5c496ce4d31f0
Cesar Parra <cesar.parra@gmail.com>
2025-07-25 05:59:58 -0400

    Refactor packaging scripts and remove deprecated project configuration

--- scripts/prepare-for-packaging.mjs
+++ scripts/prepare-for-packaging.mjs
@@ -18,19 +18,6 @@ if (fs.existsSync(projectJsonPackagingPath)) {
   const data = fs.readFileSync(projectJsonPackagingPath, 'utf8');
   const json = JSON.parse(data);
 
-  const corePackage = json.packageDirectories.find((pkg) => pkg.package === 'Expression');
-
-  // Update versionName
-  const versionNameParts = corePackage.versionName.split(' ');
-  const versionNumber = parseFloat(versionNameParts[1]);
-  corePackage.versionName = `${versionNameParts[0]} ${versionNumber + 0.01}`;
-
-  // Update versionNumber
-  const versionNumberParts = corePackage.versionNumber.split('.');
-  const majorVersion = parseInt(versionNumberParts[0]);
-  const minorVersion = parseInt(versionNumberParts[1]);
-  corePackage.versionNumber = `${majorVersion}.${minorVersion + 1}.0.NEXT`;
-
   // Save the updated file
   fs.writeFileSync(projectJsonPackagingPath, JSON.stringify(json, null, 2), 'utf8');
   console.log(`Updated ${projectJsonPackaging}`);
